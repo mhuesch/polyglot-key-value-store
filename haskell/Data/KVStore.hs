ac9d03ac4fdc9961b2b4f5b77a35b64ff27ba175
Michael Hueschen <michaelhueschen@gmail.com>
2018-03-11 18:49:10 -0700

    BitCask style k-v store, implemented in Haskell

---
+++ haskell/Data/KVStore.hs
@@ -0,0 +1,111 @@
+module Data.KVStore where
+
+
+import           Data.Binary
+import           Data.Binary.Get
+import           Data.Binary.Put
+import qualified Data.ByteString.Lazy as BS
+import           Data.IORef
+import           Data.Int
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           System.Directory
+import           System.IO
+
+
+data KVStore = KVStore
+  { handle :: Handle
+  , dictRef :: IORef Dict
+  }
+
+printKVStore :: KVStore -> IO ()
+printKVStore (KVStore hdl dict) = do
+  print hdl
+  mp <- readIORef dict
+  print mp
+
+
+type Key = String
+type Val = String
+type Dict = Map Key ValStruct
+
+data ValStruct = ValStruct { valSLen :: Int64, valSOffset :: Int64 }
+               deriving (Show)
+data ValHeader = ValHeader { keyLen :: Int64, valLen :: Int64 }
+
+instance Binary ValHeader where
+  get = do
+    keyL <- getInt64be
+    valL <- getInt64be
+    return (ValHeader keyL valL)
+
+  put (ValHeader keyL valL) = do
+    putInt64be keyL
+    putInt64be valL
+
+
+kvSet :: KVStore -> Key -> Val -> IO ()
+kvSet (KVStore hdl dictR) key val = do
+  let keyBS = runPut (put key)
+      valBS = runPut (put val)
+      keyL = BS.length keyBS
+      valL = BS.length valBS
+  hSeek hdl SeekFromEnd 0
+  BS.hPut hdl (runPut (put keyL))
+  BS.hPut hdl (runPut (put valL))
+  BS.hPut hdl keyBS
+  offset <- hTell hdl
+  BS.hPut hdl valBS
+  modifyIORef' dictR (M.insert key (ValStruct (fromIntegral valL) (fromIntegral offset)))
+  hFlush hdl
+
+
+kvGet :: KVStore -> Key -> IO (Maybe Val)
+kvGet (KVStore hdl dictR) key = do
+  dict <- readIORef dictR
+  case M.lookup key dict of
+    Nothing -> return Nothing
+    Just valStruct -> do
+      hSeek hdl AbsoluteSeek (fromIntegral (valSOffset valStruct))
+      valBS <- BS.hGet hdl (fromIntegral (valSLen valStruct))
+      return (Just (runGet get valBS))
+
+kvDir :: FilePath
+kvDir = "kv.db"
+
+openKVStore :: Maybe FilePath -> IO KVStore
+openKVStore Nothing = do
+  createDirectoryIfMissing True kvDir
+  (fp,hdl) <- openTempFile kvDir "db"
+  dict <- newIORef M.empty
+  return (KVStore hdl dict)
+--
+openKVStore (Just fp) = do
+  hdl <- openFile fp ReadWriteMode
+  dict <- newIORef =<< extractDict hdl
+  return (KVStore hdl dict)
+
+closeKVStore :: KVStore -> IO ()
+closeKVStore (KVStore hdl _) = do
+  hClose hdl
+
+extractDict :: Handle -> IO Dict
+extractDict hdl = go M.empty
+  where
+    go dict = do
+      isEnd <- hIsEOF hdl
+      if isEnd
+         then return dict
+         else do
+           let headerLen = 16
+           headerBS <- BS.hGet hdl headerLen
+           let (ValHeader keyLen valLen) = runGet get headerBS
+           --                    \/ this might be an issue on non-64-bit
+           --                       machines
+           keyBS <- BS.hGet hdl (fromIntegral keyLen)
+           let key = runGet get keyBS
+           valOffset <- fromIntegral <$> hTell hdl
+           let valStruct = ValStruct valLen valOffset
+           hSeek hdl RelativeSeek (fromIntegral valLen)
+           go (M.insert key valStruct dict)
+
---
+++ haskell/README.md
@@ -0,0 +1,24 @@
+# haskell
+
+- install [stack](https://docs.haskellstack.org/en/stable/README/)
+- to build: `stack build`
+
+## design
+
+based on [Basho's BitCask](http://basho.com/wp-content/uploads/2015/05/bitcask-intro.pdf). the file contains a sequence of records of the form
+
++--------+--------+-----------------+---------------------+
+| keyLen | valLen | key             | val                 |
++--------+--------+-----------------+---------------------+
+  8        8        keyLen            valLen
+  ^ bytes
+
+a mapping is maintained in-memory (using a `Data.Map.Map`) from Keys to ValStructs (which contains the offset in the file at which the value starts, and the length of the value in bytes). this setup allows us to "get" a value with only one disk seek and a read of a known number of bytes. "set"-ing a value requires us to serialize the keys & values, count their lengths, append them onto the end of the file, and update the in-memory dictionary. thus it requires only one disk seek, to the end of the file, and a write.
+
+currently we expose a library. it would be simple to create a command line or HTTP interface to the store.
+
+other TODOs:
+- support datatypes other than strings
+- support deletion of keys (using tombstones)
+- support compaction of the db file
+- support multiple files
